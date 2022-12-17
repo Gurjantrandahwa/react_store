7aec17b4df6e1263de80b7fb71f2b52d60e3036d
Gurjant Singh <gurjantrandhawa74456@gmail.com>
2022-12-17 20:49:39 +0530

    ecommerce

--- src/Components/FeatureProducts/FeatureProducts.js
+++ src/Components/FeatureProducts/FeatureProducts.js
@@ -1,17 +1,30 @@
 import React from "react";
 import "./FeatureProduct.scss";
 import {useProductContext} from "../../Common/Context/productContext";
+import {CircularProgress, Typography} from "@mui/material";
 
 
 export default function FeatureProducts() {
     const {isLoading, featureProducts} = useProductContext();
     console.log("feature", featureProducts)
-    return <div className={"features-products container"}>
+    if (isLoading) {
+        return <div className={"loader"}>
+            <CircularProgress color="secondary"/>
+        </div>
+    }
+    return <div className={"container feature-container"}>
+        <Typography variant={"caption"}>
+            Our Features Products
+        </Typography>
+        <div className={"features-products"}>
 
-        {featureProducts.map((value, index) => {
-            return <div key={index}>
-                <img src={value.image} alt={"feature"} width={200}/>
-            </div>
-        })}
+            {featureProducts.map((value, index) => {
+                return <div key={index}>
+                    <img src={value.image} alt={"feature"} width={200}/>
+                </div>
+            })}
+        </div>
     </div>
+
+
 }
